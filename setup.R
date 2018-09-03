e50659993d534faafee22d245342332f02ced931
Maximilian Held <info@maxheld.de>
2018-09-03 15:34:59 +0200

    add google trends

--- setup.R
+++ setup.R
@@ -1,13 +1,16 @@
 # dependencies ====
 library(knitr)
 library(rmarkdown)
+library(printr)
+library(ggplot2)
 
 # knitr setup ====
 knitr::opts_chunk$set(
   tidy = TRUE,   # tidy formats code nicely in echo
   cache = TRUE,
   echo = FALSE,
-  message = FALSE
+  message = FALSE,
+  out.width = '100%'
 )
 options(digits = 2)  # display only 2 digits in knitr output
 options(scipen = 999)
